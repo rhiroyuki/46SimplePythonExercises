c3c8452b5e31a3857a4f91e741e327299ca7ea65
ricardohiroyuki <ricardo.hiroyuki@Outlook.com>
2015-05-19 21:15:41 -0300

    Updated ex01

--- exercise01.py
+++ exercise01.py
@@ -1,12 +1,14 @@
+"""
+Define a function max() that takes two numbers 
+as arguments and returns the largest of them. 
+Use the if-then-else construct available in Python. 
+(It is true that Python has the max() function 
+built in, but writing it yourself is nevertheless 
+a good exercise.)
+"""
+
 def max(number1, number2):
 	if number1 > number2:
 		return number1
 	else:
 		return number2;
-
-#some tests
-if __name__ == "__main__":
-	print(max(2, 3))
-	print(max(3, 2))
-	print(max(3.2, 2.3))
-	print(max(2.3, 3.2))
