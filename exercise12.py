31d250243ca8f182ff4aac9f2031c8657a54545f
ricardohiroyuki <ricardo.hiroyuki@Outlook.com>
2015-05-29 13:48:54 -0300

    another exercise

---
+++ exercise12.py
@@ -0,0 +1,16 @@
+"""
+Define a procedure histogram() that takes a list of integers and prints a histogram to the screen. 
+For example, histogram([4, 9, 7]) should print the following:
+
+****
+*********
+*******
+"""
+
+def histogram2(intList):
+	for number in intList:
+		print('*' * number)
+
+
+if __name__ == '__main__':
+	histogram([4, 9, 7])
