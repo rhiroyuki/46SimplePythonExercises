d7cbe1db9fbb313cbb09ecfc75340024a2d54edb
rhiroyuki <ricardo.hiroyuki@hotmail.com>
2016-07-02 18:16:34 -0300

    Implementa cores no modulo de teste

--- testModule.py
+++ testModule.py
@@ -1,30 +1,15 @@
+from colorama import init, Fore
+
 class UnitTester(object):
 	"""Class created to test functions"""
 	def __init__(self):
 		self.testes = 0
 
 	def do_test(self, value, expected):
+		init() 	# Windows compatibility with colored words
 		self.testes += 1
 		if value == expected:
-			print('Test %d passed!' %self.testes)
+			print(Fore.GREEN + 'Test %d PASSED!' %self.testes)
 		else:
-			print('Test', self.testes,'Error, expected', expected,'found:', value)
+			print(Fore.RED + 'Test', self.testes,'ERROR!', Fore.RESET + 'Expected: "', expected,'" and Found: "', value, '"')
 
-#def soma(a, b):
-#	return a+b
-
-#def reversedList(lista):
-#	return lista[::-1]
-
-#soma(1,2,3)
-#teste = UnitTester()
-#teste.test(soma(1,2), 3)
-#teste.test(soma(3,5), 8)
-#teste.test(soma(1,2), 0)
-#teste.test(soma(1,2), 3)
-#teste.test(soma(1,3), 3)
-#teste.test(soma(1,5), 3)
-
-#teste2 = UnitTester()
-#teste2.test(reversedList('abacate'), 'etacaba')
-#teste2.test(reversedList([1,2,3,45,321,6]), [6, 321, 45, 3, 2, 1])
