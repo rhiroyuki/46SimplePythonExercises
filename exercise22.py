f873b4960ffef803264fcb83543127c345664ae2
rhiroyuki <ricardo.hiroyuki@hotmail.com>
2016-09-01 19:03:50 +0000

    Adiciona exercicio 22

---
+++ exercise22.py
@@ -0,0 +1,24 @@
+import testModule
+import string
+
+key = {'a':'n', 'b':'o', 'c':'p', 'd':'q', 'e':'r', 'f':'s', 'g':'t', 'h':'u',
+       'i':'v', 'j':'w', 'k':'x', 'l':'y', 'm':'z', 'n':'a', 'o':'b', 'p':'c',
+       'q':'d', 'r':'e', 's':'f', 't':'g', 'u':'h', 'v':'i', 'w':'j', 'x':'k',
+       'y':'l', 'z':'m', 'A':'N', 'B':'O', 'C':'P', 'D':'Q', 'E':'R', 'F':'S',
+       'G':'T', 'H':'U', 'I':'V', 'J':'W', 'K':'X', 'L':'Y', 'M':'Z', 'N':'A',
+       'O':'B', 'P':'C', 'Q':'D', 'R':'E', 'S':'F', 'T':'G', 'U':'H', 'V':'I',
+       'W':'J', 'X':'K', 'Y':'L', 'Z':'M'}
+
+def caeserROT13(inputStr):
+    outStr = ''.join([key[char] if char in string.ascii_letters else char for char in inputStr])
+    return outStr
+
+if __name__ == '__main__':
+    test = testModule.UnitTester()
+    test.do_test(caeserROT13('a'), 'n')
+    test.do_test(caeserROT13('a '), 'n ')
+    test.do_test(caeserROT13('Pnrfne pvcure? V zhpu cersre Pnrfne fnynq!'), 'Caesar cipher? I much prefer Caesar salad!')
+    test.do_test(caeserROT13('!!!!!!!!!!!!!!!!!!'), '!!!!!!!!!!!!!!!!!!')
+    test.do_test(caeserROT13(''), '')
+    test.do_test(caeserROT13('1234567890'), '1234567890')
+    test.do_test(caeserROT13('abcde'), 'nopqr')
